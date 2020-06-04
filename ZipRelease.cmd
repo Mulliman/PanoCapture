312231f176d0f68dc17945943f3a10e7ad69f47d
Sam Mullins <sammullins.co.uk@gmail.com>
2020-06-04 08:26:19 +0100

    Updated the docs so that the plugin can be downloaded.

---
+++ ZipRelease.cmd
@@ -0,0 +1,14 @@
+echo "Have you run a release build on PanoCapture.Plugin?"
+
+pause
+
+cd ".\PanoCapture\Plugin\bin\Release"
+
+powershell Compress-Archive ./* "..\..\..\..\docs\assets\releases\PanoCapture.CaptureOne.zip" -force
+
+cd "..\..\..\..\docs\assets\releases"
+
+call del PanoCapture.CaptureOne.coplugin
+call RENAME PanoCapture.CaptureOne.zip PanoCapture.CaptureOne.coplugin
+
+pause
